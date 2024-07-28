dc0426ca1d96ddf5cfbe6e5a26bb867c25259db8
Louie Alfonso <lfriasalfonso@gmail.com>
2024-07-28 21:08:26 +0100

    Add Server & MongoDb Setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import { dbConnection } from "./utils/index.js";
+
+dotenv.config();
+dbConnection();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));

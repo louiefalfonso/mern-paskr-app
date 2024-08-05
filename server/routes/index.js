d57c75d698ff01dbec08bbf401793e8a823b8c62
Louie Alfonso <79123140+louiefalfonso@users.noreply.github.com>
2024-08-05 15:16:46 +0100

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,6 @@ import taskRoutes from "./taskRoutes.js";
 const router = express.Router();
 
 router.use("/user", userRoutes);
-router.use("/task", taskRoutes);
+router.use("/tasks", taskRoutes);
 
 export default router;

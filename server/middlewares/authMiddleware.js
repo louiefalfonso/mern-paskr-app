df5dd3affec58e799bec7b15ceb462619aee834e
Louie Alfonso <79123140+louiefalfonso@users.noreply.github.com>
2024-08-05 18:03:52 +0100

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,51 +3,49 @@ import User from "../models/user.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    let token = req.cookies?.token;
+    const token = req.cookies?.token;
 
-    if (token) {
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      console.log("decodedToken:", decodedToken);
-
-      const resp = await User.findById(decodedToken.userId).select(
-        "isAdmin email"
-      );
-      console.log("resp:", resp);
-
-      if (resp) {
-        req.user = {
-          email: resp.email,
-          isAdmin: resp.isAdmin,
-          userId: decodedToken.userId,
-        };
-        next();
-      } else {
-        return res
-          .status(401)
-          .json({ status: false, message: "User not found" });
-      }
-    } else {
-      return res
-        .status(401)
-        .json({ status: false, message: "Not authorized. Try login again." });
+    if (!token) {
+      return res.status(401).json({
+        status: false,
+        message: "Not authorized. Try login again.",
+      });
     }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decodedToken.userId).select(
+      "isAdmin email"
+    );
+
+    if (!user) {
+      return res.status(401).json({ status: false, message: "User not found" });
+    }
+
+    req.user = {
+      email: user.email,
+      isAdmin: user.isAdmin,
+      userId: decodedToken.userId,
+    };
+
+    next();
   } catch (error) {
     console.error(error);
-    return res
-      .status(401)
-      .json({ status: false, message: "Not authorized. Try login again." });
+    return res.status(401).json({
+      status: false,
+      message: "Not authorized. Try login again.",
+    });
   }
 };
 
 const isAdminRoute = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
+  if (!req.user || !req.user.isAdmin) {
     return res.status(401).json({
       status: false,
       message: "Not authorized as admin. Try login as admin.",
     });
   }
+
+  next();
 };
 
 export { isAdminRoute, protectRoute };

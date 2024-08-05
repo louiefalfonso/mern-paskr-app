f4a27aa6c85fcaa70fad074d773a0622b1cd9f13
Louie Alfonso <79123140+louiefalfonso@users.noreply.github.com>
2024-08-05 15:31:22 +0100

    Update taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -13,18 +13,23 @@ import {
 
 const router = express.Router();
 
-router.post("/create", createTask);
-router.put("/update/:id", updateTask);
+router.post("/create", protectRoute, isAdminRoute, createTask);
+router.put("/update/:id", protectRoute, isAdminRoute, updateTask);
 router.get("/alltasks", protectRoute, getTasks);
 
-router.post("/activity/:id", postTaskActivity);
+router.post("/activity/:id", protectRoute, postTaskActivity);
 router.get("/dashboard", protectRoute, dashboardStatistics);
-router.get("/", getTasks);
-router.get("/:id", getTask);
+router.get("/", protectRoute, getTasks);
+router.get("/:id", protectRoute, getTask);
 
 
-router.put("/:id", trashTask);
+router.put("/:id", protectRoute, isAdminRoute, trashTask);
 
-router.delete("/delete-restore/:id?", deleteRestoreTask);
+router.delete(
+  "/delete-restore/:id?",
+  protectRoute,
+  isAdminRoute,
+  deleteRestoreTask
+);
 
 export default router

e04f485bc08907b17c09fb0038c1c9ca4e7cf3fd
Louie Alfonso <79123140+louiefalfonso@users.noreply.github.com>
2024-08-05 17:06:36 +0100

    Update taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -18,7 +18,7 @@ router.put("/update/:id", protectRoute, isAdminRoute, updateTask);
 router.get("/alltasks", protectRoute, isAdminRoute, getTasks);
 
 router.post("/activity/:id", protectRoute, postTaskActivity);
-router.get("/dashboard", protectRoute, isAdminRoute, dashboardStatistics);
+router.get("/dashboard", protectRoute, dashboardStatistics);
 router.get("/", protectRoute, getTasks);
 router.get("/:id", protectRoute, getTask);
 
